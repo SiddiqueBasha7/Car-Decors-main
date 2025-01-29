c0e8b06619f0febb6221a402d0bf96eb5cf2db11
Siddiquebasha7 <siddiquebasha7@gmail.com>
2025-01-29 15:40:09 +0530

    modified:   index.html 	new file:   script.js 	new file:   style.css

---
+++ script.js
@@ -0,0 +1,16 @@
+// Custom JS Script
+document.addEventListener('DOMContentLoaded', () => {
+const sections = document.querySelectorAll('.scroll-effect');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+    if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+    }
+    });
+}, { threshold: 0.1 });
+
+sections.forEach(section => {
+    observer.observe(section);
+});
+});
